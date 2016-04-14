31e15113854a5028aa0f9cdc5c4cac22d489f6aa
Rizky Luthfianto <mrluthfianto@gmail.com>
2016-04-15 00:34:11 +0700

    .py untuk generate datasets yang siap train

---
+++ make_datasets.py
@@ -0,0 +1,95 @@
+﻿# Not 100% finished yet
+
+from __future__ import division
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+LABEL = 'returnQuantity'
+
+def preprocess(df):
+	# Months
+	df['mmdd'] = df.orderDate.str[-5:]
+	df['months'] = df.orderDate.str[-5:-3].astype(np.int8)
+
+	# Total price for an order
+	order = df[['orderID', 'price']].groupby('orderID').sum()
+	order.columns = ['total_order']
+	df = df.set_index('orderID').join(order).reset_index()
+	del order
+
+	# Average budget of the customer
+	budget = df[['customerID', 'total_order']].groupby('customerID').mean()
+	budget.columns = ['budget']
+	df = df.set_index('customerID').join(budget).reset_index()
+	del budget
+
+	# Return probability. 0.5 if unknown, assuming fairness
+	df_return_probability = df[['articleID', 'returnQuantity', 'quantity']].groupby('articleID').sum()
+	df_return_probability['return_probability'] = df_return_probability.returnQuantity / df_return_probability.quantity
+	return_prob_dict = df_return_probability['return_probability'].to_dict()
+	del df_return_probability
+	df['return_prob'] = df.articleID.apply(lambda x: return_prob_dict.get(x, 0.5))
+
+	# Price after rebate = total_order - voucherAmount
+	df['after_voucher'] = df.total_order - df.voucherAmount
+
+	# Orders (as in rank)
+	df['order_order']  = df[['customerID', 'orderID']].groupby(['customerID']).cumcount()
+	df['choice_order'] = df[['orderID', 'articleID']].groupby(['orderID']).cumcount()
+
+	# Is it wednesday?
+	print("[SLOW] Is it wednesday?")
+	df['wednesday'] = df.orderDate.apply(pd.to_datetime).apply(lambda x: x.weekday())
+	df.wednesday[df.wednesday != 2] = 0
+	df.wednesday[df.wednesday == 2] = 1
+
+	# Iseng ubah float ke integer. Integer aja, asik, enteng, wkwk
+	df.price = df.price.astype(np.int32)
+	df.budget = df.budget.astype(np.int32)
+
+	# Konversi kategori/object ke numerik
+	print("Konversi kategori/object ke numerik:")
+
+	# Cari kolom yang tipenya object, bukan integer maupun float
+	object_columns = df.loc[:, df.dtypes == object].columns
+
+	# orderID would never be repeated
+	del df['orderID']
+
+	for col in object_columns:
+	    print(col)
+	    le=LabelEncoder()
+	    # Konversi deh
+	    df[col]=le.fit_transform(df[col])
+
+	# Unused functions
+	# df['cSizeCode'] = df.sizeCode.apply(changeSizeCode)
+	# df['season'] = df.months.apply(getSeason)
+	# df['priceDiff'] = df['rrp'] - (df['price'] / df['quantity'])
+	# df['gpriceDiff'] = df['priceDiff'].apply(grouppriceDiff)
+	# df['price_delta']=df.rrp-df.price;# df.price_delta.head()
+
+	return df
+
+def main():
+    train_df = pd.read_csv('orders_train.txt', sep=';')
+    tests_df = pd.read_csv('orders_class.txt', sep=';')
+
+    # split based on the end of train_df index
+    split = train_df.shape[0]
+
+    df = pd.concat([train_df, tests_df], axis=0, ignore_index=True)
+    df = preprocess(df)
+    # df = df.drop(['orderID'], axis=1)
+
+    train_df = df[:split]
+    tests_df = df[split:]
+
+    tests_df = tests_df.drop(LABEL, axis=1)
+
+    train_df.to_csv('train_gue.csv', index=False)
+    tests_df.to_csv('tests_gue.csv', index=False)
+
+if __name__ == "__main__":
+    main()
